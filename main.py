bd796f1e76f7702566d0082b302e2a5cb670b416
Abed <freazer122@gmail.com>
2022-05-07 21:19:51 +0300

    aproximately done

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ from create_problem_sets import nearest_neighbour,clark_wright
 from settings import *
 import time
 # algo = {GenA: GA_LAB1, ISLAND: PureMA, ACO_PAR: ACO, SA: simulated_anealling, TS: Tabu_search, CO_PS: CO_PSO}
-algo = {GenA: GA_LAB1, 2: ACO_alg,3:simulated_annealing,4:tabu }
+algo = {1: GA_LAB1, 2: ACO_alg,3:simulated_annealing,4:tabu }
 tags = {1: "GA_CX_SWAP", 2: "ACO",3:"simulated_annealing",4:"tabu" }
 heuristics={1: "NN", 2: "C&W"}
 mutation_index={2:"SWAP",3:"INSERT"}
 problem_sets_GA = {1: nearest_neighbour, 2: clark_wright}
 # problem_sets_PSO = {BUL_PGIA: PSO_prb}
-inputs_for_testing=["E-n22-k4", "E-n33-k4","E-n51-k5",  "E-n76-k8",  "E-n76-k10",
+inputs_for_testing=["","E-n22-k4", "E-n33-k4","E-n51-k5",  "E-n76-k8",  "E-n76-k10",
            "E-n101-k8",  "E-n101-k14"]
 def plot(fitness, iter,tag,names):
 
@@ -25,8 +25,8 @@ def plot(fitness, iter,tag,names):
     plt.xlabel('iterations')
     plt.title(inputs_for_testing[tag])
     plt.legend()
-    plt.show()
-
+    plt.savefig(f"outputs\{inputs_for_testing[tag]}\{inputs_for_testing[tag]}-iter{len(iter[0])}.png")
+    plt.close()
 
 
 inputs = {1: "E-n22-k4.txt", 2: "E-n33-k4.txt", 3: "E-n51-k5.txt", 4: "E-n76-k8.txt", 5: "E-n76-k10.txt",
@@ -80,50 +80,70 @@ def get_sets_from_files(name):
     return cities, cost_matrix, dimentions, capacity
 
 def create_results_file(name):
-    file = open(fr"outputs\{name}", "r")
-    f = open(fr"newoutput\{name}", "w+")
-    for _ in range(50):f.write("-")
+    file = open(fr"outputs\{name}\script_output.txt", "r")
+    f = open(fr"outputs\Results.txt", "a")
 
     lines = file.readlines()
     lines = [x.split(' ') for x in lines]
-    spaceforfirst=20
+    spaceforfirst=15
+    fitnes=23
+    all_lines=fitnes*3+spaceforfirst
+    for _ in range(all_lines-2):f.write("-")
+    mid=all_lines//2-len(name)//2
+    f.write("\n")
+    for _ in range(mid):f.write(" ")
+    f.write(f"|{name}|")
+    f.write("\n| Algorithem   |       fitness         |         Time          |      ticks      |\n")
+    for _ in range(all_lines-2):f.write("-")
     for index,line in enumerate(lines):
-        if line[:2]=="GA" or line[:2]=="TS" or line[:2]=="SA" or line[:2]=="AC":
-            f.write("|")
-            f.write(str(line))
-            for i in range(len(line)-spaceforfirst):
+
+        if str(line[0][:2])=="GA" or line[0][:2]=="TS" or line[0][:2]=="SA" or line[0][:2]=="AC":
+            f.write("\n|")
+            f.write(str(line[0][:len(line[0])-3]))
+            for i in range(spaceforfirst-len(line[0])+2):
                 f.write(" ")
             f.write("|")
-        if line[1:3]=="fi":
-            f.write("|")
-            f.write(str(lines[index+1]))
-            f.write("|")
-        if line[:3] == "Ti":
-            f.write("|")
-            f.write(str(lines[index + 1]))
-            f.write("|")
-        if line[:3] == "ti":
-            f.write("|")
-            f.write(str(lines[index + 1]))
-            f.write("|")
+        if len(line)>2 and len(line[2])>3:
+            print(line)
+            if str(line[1])==",fittness:":
+                f.write(str(line[2]))
+                for i in range(fitnes - len(line[2])):
+                    f.write(" ")
+                f.write("|")
+        if len(line) > 3 and len(line[2]) > 3:
+            if str(line[4]) == "Time":
+                f.write(str(line[7]))
+                for i in range(fitnes - len(line[7])):
+                    f.write(" ")
+                f.write("|")
+        if len(line) > 8 and len(line[2]) > 3:
+            if str(line[9]) == "ticks:":
+
+                f.write(str(line[10][:len(line[10])-3]))
+                for i in range(fitnes - len(line[10])-3):
+                    f.write(" ")
+                f.write("|\n")
+                for _ in range(all_lines-2): f.write("-")
 
 
-def ga_script():
-    GA_POPSIZE=300
-    max_iter=80
+
+def ga_script(iterations,popsize):
+    GA_POPSIZE=popsize
+    max_iter=iterations
+    Gene_dist = 4
+
+    names = []
+    select_generator = 1
+    problem_set = problem_sets_GA[select_generator]
+
     for select_input in range(1,len(inputs.keys())+1):
         fitness_arr,iter_array=[],[]
 
-        # generators
+        for select_generator in range(1, 3):
+            cities, cost_matrix, dimentions, capacity = get_sets_from_files(inputs[select_input])
+            GA_TARGET = [cities, cost_matrix, dimentions, capacity]
+            target_size = len(cities)
 
-        # mutation
-        Gene_dist = 4
-        cities, cost_matrix, dimentions, capacity = get_sets_from_files(inputs[select_input])
-        GA_TARGET = [cities, cost_matrix, dimentions, capacity]
-        target_size = len(cities)
-        names=[]
-        for select_generator in range(1,3):
-            problem_set = problem_sets_GA[select_generator]
             for mutation in range(2,4):
 
                 print("GA_I_"+heuristics[select_generator]+"_"+mutation_index[mutation])
@@ -158,57 +178,65 @@ def ga_script():
             fitness_arr.append(fitness)
             iter_array.append(iteration)
             names.append("ACO_" + heuristics[select_generator] )
-
         plot(fitness_arr, iter_array, select_input,names)
 
-
+def border():
+    print("----------------------------------")
 def main():
 
     process = True
-    Gene_dist = 4
-    GA_TARGET = None
+
+    select_input = int(input("select data set:\n1: E-n22-k4.txt\n2: E-n33-k4.txt\n3: E-n51-k5.txt\n4: E-n76-k8.txt\n5: E-n76-k10.txt"
+                             "\n6: E-n101-k8.txt\n7: E-n101-k14.txt"))
+    border()
     while process:
-        '''
-        get input
-        pop size
-        select input 
-        generators 
-        mutation
-        algo 
-        '''
-        # get_sets_from_files(name)
-        # default vals
+
         serviving_stratigy = 1  # elite
         crosstype = CX_
-        # pop size
-        GA_POPSIZE = int(input("set population size:"))
-        max_iter=int(input("number of iterations:"))
-        print("max",max_iter)
-        select_input = int(input("1: E-n22-k4.txt, 2: E-n33-k4.txt, 3: E-n51-k5.txt, 4: E-n76-k8.txt, 5: E-n76-k10.txt,"
-                                 "6: E-n101-k8.txt, 7: E-n101-k14.txt"))
-        # target ,from select_input
+        mutation=1
+        Gene_dist = 4
+        GA_POPSIZE=1
         cities, cost_matrix, dimentions, capacity = get_sets_from_files(inputs[select_input])
         GA_TARGET = [cities, cost_matrix, dimentions, capacity]
         target_size = len(cities)
 
-        select_generator = int(input("select construction :  1:clark write  2:nearest neigbour"))
-        # generators
-        problem_set = problem_sets_GA[select_generator]
-        # mutation
-        mutation = int(
-            input("choose mutation scheme:  random mutation: 1 ,swap_mutate: 2 ,insertion_mutate: 3"))
-        # cho
-        alg = int(input("chose algorithem :\n1:Island GA \n2: Ant Colony Optimization \n3: simulated annealing\n4: tabu search"))
+        # pop size
+        border()
 
-        # solution = GA_LAB1(GA_TARGET, target_size, GA_POPSIZE, problem_set, crosstype, "fitness", 3,
-        #                      serviving_stratigy, mutation, Gene_dist,max_iter=max_iter)
-        solution = algo[alg](GA_TARGET, target_size, GA_POPSIZE, problem_set,"fitness",max_iter,mutation)
+        alg = int(input("chose algorithem :\n1:Island GA \n2: Ant Colony Optimization \n3: simulated annealing\n4: tabu search"))
+        border()
+
+        max_iter = int(input("number of iterations:"))
+
+        select_generator=int(input("select heuristic: \n1: nearest_neighbour \n2: clark_wright"))
+        border()
+
+        problem_set = problem_sets_GA[select_generator]
+        if alg==1 or alg==4 or alg==3:
+            border()
+
+            mutation = int(input("select mutation:\n1: swap\n2: insert"))+1
+
+        if alg==1:
+            border()
+
+            GA_POPSIZE = int(input("set population size:"))
+
+            solution =GA_LAB1(GA_TARGET, target_size, GA_POPSIZE, problem_set, CX_, "fitness", 3,
+                    1, mutation, Gene_dist, max_iter=max_iter)
+        elif alg==3 or alg==4:
+            solution = algo[alg](GA_TARGET, target_size, GA_POPSIZE, problem_set, "fitness", max_iter, mutation)
+        else:
+            solution = algo[2](GA_TARGET, target_size, GA_POPSIZE, problem_set, "fitness", max_iter)
+
 
         overall_time = time.perf_counter()
         solution.solve()
         overall_time = time.perf_counter() - overall_time
+        border()
         print(solution.max_iter)
         print("Overall runtime :", overall_time)
+        border()
 
         print("\n run again ? press y for yes n for no ")
         if input() == "n":
@@ -216,4 +244,13 @@ def main():
 
 
 if __name__ == "__main__":
-    ga_script()
+    selector=int(input("select manual settings or test of all algorithms:\n1: manual  \n2:automatic test"))
+    border()
+    if selector==1:
+        main()
+    else:
+        border()
+        popsize = int(input("enter population size for all tests:"))
+        border()
+        iterations=int(input("enter  number of max iterations:"))
+        ga_script(iterations,popsize)
